2c2d1ae2a4c9f6f33f42ba27f0bd1bdefa59a927
DLMousey <tom@llamatorials.com>
2019-01-12 00:57:46 +0000

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,34 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
+    // Generate a random number using thread seeded RNG
     let secret_number = rand::thread_rng().gen_range(1, 101);
     let mut num_tries = 1;
+
     println!("Guess the number");
 
+    // Start up a loop, allows game to keep running while user is guessing
     loop {
         println!("Please input your guess.");
 
+        // Create a mutable string variable to hold the user's guess
         let mut guess = String::new();
 
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
 
+        /*
+            Shadow the guess string with a u32 variable of the same name,
+            This allows us to use match to properly compare it's value
+        */
         let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue
+            Ok(num) => num, // Return value if operation was successful
+            Err(_) => continue // Junk out non integer input, return to top of loop
         };
 
         println!("You guessed: {}", guess);
 
+        // Compare the shadowed version of guess
         match guess.cmp(&secret_number) {
             Ordering::Less => {
                 println!("Too small!");
@@ -38,7 +47,7 @@ fn main() {
                     println!("You win! It only took you 1 attempt, Great job!");
                 }
 
-                break;
+                break; // Exit the loop once the user guesses right
             }
         }
     }
